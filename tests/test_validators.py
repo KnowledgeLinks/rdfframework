ccbc6dac060def554c543e61cb06637ae2c4af1e
Jeremy Nelson <jermnelson.gmail.com>
2016-04-03 14:44:13 -0600

    added new test_processors

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,57 +1,15 @@
-##__author__ = "Jeremy Nelson, Mike Stabile"
-##
-##import os
-##import sys
-##import unittest
-##
-##from rdfframework.validators import *
-##from rdfframework.processors import csv_to_multi_prop_processor, \
-##    email_verification_processor, password_processor, run_processor,\
-##    salt_processor
-##    
-##
-##class Test_csv_to_multi_prop_processor(unittest.TestCase):
-##
-##    def setUp(self):
-##        self.tags = {
-##            "comment": "Tags for the badges.",
-##            "propertyProcessing": "http://knowledgelinks.io/ns/data-resources/CSVstringToMultiPropertyProcessor",
-##            "range": [
-##            {
-##              "storageType": "literal",
-##              "rangeClass": "http://www.w3.org/2001/XMLSchema#string"
-##            }
-##            ],
-##           "propUri": "https://w3id.org/openbadges#tags",
-##           "type": "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"}
-##      
-##
-##    def test_load_mode(self):
-##        self.tags["dataValue"] = ["red", "green", "blue", "yellow"]
-##        #result = csv_to_multi_prop_processor(self.tags, "load")
-##        #self.assertEqual(result,
-##        #    "red, green, blue, yellow")
-##
-##
-##    def test_save_mode(self):
-##        self.tags["prop"] = {"new": "red, green, blue, yellow"}
-##        self.tags["processedData"] = {}
-##        #result = csv_to_multi_prop_processor(self.tags)
-##        #self.assertTrue(result['prop']['calcValue'])
-##        #self.assertListEqual(
-##        #    sorted(result['processedData'][self.tags.get('propUri')]),
-##        #    sorted(["red", "green", "blue", "yellow"]))
-##        #result2 = csv_to_multi_prop_processor(self.tags, "save")
-##        #self.assertEqual(result, result2)
-##
-##
-##    def test_unknown_mode(self):
-##        pass
-##        #! Should an unknown mode raise an error instead of returning the
-##        #! object?
-##        #self.assertEqual(
-##        #    self.tags,
-##        #    csv_to_multi_prop_processor(self.tags, "unknown"))
+__author__ = "Jeremy Nelson, Mike Stabile"
+
+import os
+import sys
+import unittest
+
+from rdfframework.validators import *
+from rdfframework.processors import csv_to_multi_prop_processor, \
+    email_verification_processor, password_processor, run_processor,\
+    salt_processor
+    
+
 ##
 ##
 ##
