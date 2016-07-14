d2f4ae6eeaf86d8c0151accf01e371c9de88b551
Jeremy Nelson <jermnelson.gmail.com>
2016-07-14 07:44:20 -0600

    Adding unit test stub for RDF Namespace Manager

---
+++ tests/test_rdfnsmanager.py
@@ -0,0 +1,11 @@
+__author__ = "Jeremy Nelson, Mike Stabile"
+
+import unittest
+import rdffframework.utilities.uriconvertor as uriconvertor 
+
+class TestConvertToNs(unittest.TestCase):
+
+    def test_default_conversion(self):
+        pass
+        
+
