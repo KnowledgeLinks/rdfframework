d6a47447ebad8e3f0a9e8e5d62af1cca660a8219
Jeremy Nelson <jermnelson@gmail.com>
2018-09-30 16:04:15 -0600

    Added init for datatypes, starting ingesters test stubs

---
+++ tests/datatypes/__init__.py
@@ -0,0 +1,12 @@
+__author__ = "Jeremy Nelson"
+
+import unittest
+
+
+if __name__ == '__main__':
+    from test_datatypeerrors import *
+    from test_namespaces import *
+    from test_rdfdatatypes import *
+    from test_uriformatters import *
+    from test_xsdtypes import *
+    unittest.main()
