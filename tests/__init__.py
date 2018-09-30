93b8777822a0ac5c89633d49b080e00a673a1b41
Jeremy Nelson <jermnelson@gmail.com>
2018-09-30 15:57:14 -0600

    Added stubs for datamanager unit tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,9 @@ if __name__ == '__main__':
     from test_rdfwconfig import *
     from test_framework import *
     from connections.test_blazegraph import *
+    from datamanager import *
     from utilities.test_fileutilities import *
+
     from datatypes.test_rdfdatatypes import *
     from datatypes.test_namespaces import *
     unittest.main()
---
+++ tests/datamanager/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Jeremy Nelson"
+
+import unittest
+
+
+if __name__ == '__main__':
+    from test_datamanager import *
+    from test_datastoremanager import *
+    from test_defmanager import *
+    unittest.main()
