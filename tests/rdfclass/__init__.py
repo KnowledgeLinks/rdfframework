ecce521937a00ed2d17122064308fb3d11724723
Jeremy Nelson <jermnelson@gmail.com>
2018-09-30 16:13:39 -0600

    RdfClass test stubs including Elasticsearch conversion, RdfClass, and RdfProperty

---
+++ tests/rdfclass/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Jeremy Nelson"
+
+import unittest
+
+if __name__ == '__main__':
+    from test_esconversion import *
+    from test_rdfclass import *
+    from test_rdffactories import *
+    from test_rdfproperty import *
+    unittest.main()
